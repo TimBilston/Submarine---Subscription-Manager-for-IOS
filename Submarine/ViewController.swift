d9a96c5795488382fa2c257a255d945dde364434
tbil0002 <tbil0002@student.monash.edu>
2022-04-16 13:31:36 +1000

    Initial Commit

---
+++ Submarine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Submarine
+//
+//  Created by Nick Exon on 16/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
